86a988398229f314859008c04e4c421705eb84f9
Pablo Santos <opablosantos@gmail.com>
2021-02-19 22:39:59 -0300

    Iniciei a resolução do exercício

--- Aula-02-Exercicios/src/Exercicio03.java
+++ Aula-02-Exercicios/src/Exercicio03.java
@@ -1,9 +1,16 @@
+import javax.swing.JOptionPane;
 
 public class Exercicio03 {
 
 	public static void main(String[] args) {
 		// Faça um programa que receba três notas e seus respectivos pesos, 
 		// calcule e mostre a média ponderada.
+		float n1, n2, n3, media;
+		n1 = Float.valueOf(JOptionPane.showInputDialog("Nota 1: "));
+		n2 = Float.valueOf(JOptionPane.showInputDialog("Nota 2: "));
+		n3 = Float.valueOf(JOptionPane.showInputDialog("Nota 3: "));
+		media = (n1 + n2 + n3)/3;
+		JOptionPane.showMessageDialog(null, "Sua média é: " + media);
 
 	}
 
