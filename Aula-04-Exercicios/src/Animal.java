9449c038d7f642d45e0ff48b6a30d3a6824a0917
Pablo Santos <opablosantos@gmail.com>
2021-03-12 19:54:10 -0300

    Alteração na classe Animal

--- Aula-04-Exercicios/src/Animal.java
+++ Aula-04-Exercicios/src/Animal.java
@@ -1,51 +1,83 @@
 import javax.swing.JOptionPane;
-
 public class Animal {
-	//Atributos
-	String nome;
-	String classe;
-	String familia;
-	int idade;
-	int caloria;
-	int forca;
-	boolean estado;
+	// Atributos
+	private String nome;
+	private String classe;
+	private String familia;
+	private int idade;
+	private int caloria;
+	private int forca;
+	private boolean estado;
 	
-	Animal() {
+	Animal(){
 		this.nascer();
 	}
-	
 	public void nascer() {
-		nome = JOptionPane.showInputDialog("Nome: ");
-		classe = JOptionPane.showInputDialog("Classe: ");
-		familia = JOptionPane.showInputDialog("Família: ");
+		nome = JOptionPane.showInputDialog("Nome:");
+		classe = JOptionPane.showInputDialog("Classe:");
+		familia = JOptionPane.showInputDialog("Familia:");
 		idade = 0;
 		caloria = 10;
 		forca = 10;
 		estado = true;
 	}
 	
+	
+	//Getters e Setters
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getClasse() {
+		return classe;
+	}
+	public String getFamilia() {
+		return familia;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public int getCaloria() {
+		return caloria;
+	}
+	public int getForca() {
+		return forca;
+	}
+	public boolean isEstado() {
+		return estado;
+	}
+	
+	
 	public void morrer() {
-		
+		this.forca = 0;
+		estado = false;
+		JOptionPane.showMessageDialog(null, this.nome+" Morreu!!!");
 	}
-	
 	public void comer() {
-		
+		if (this.estado && this.caloria <= 100 && this.forca>=2) {
+			this.caloria += 10;
+			this.forca -= 2;
+			JOptionPane.showMessageDialog(null, this.nome+" Comeu!!!");
+		} else if (!estado){
+			JOptionPane.showMessageDialog(null, this.nome+" Está Morto!!!");
+		} else if (caloria >= 100){
+			JOptionPane.showMessageDialog(null, this.nome+" Está Cheio!!!");
+		} else {
+			JOptionPane.showMessageDialog(null, this.nome+" Sem Força!!!");
+		}
+		JOptionPane.showMessageDialog(null, this.toString());
 	}
-	
 	public void correr() {
-		
+		JOptionPane.showMessageDialog(null, this.nome+" Correu!!!");
 	}
-	
 	public void dormir() {
-		
+		JOptionPane.showMessageDialog(null, this.nome+" Dormiu!!!");
 	}
-
 	@Override
 	public String toString() {
 		return "Animal [nome=" + nome + ", classe=" + classe + ", familia=" + familia + ", idade=" + idade
 				+ ", caloria=" + caloria + ", forca=" + forca + ", estado=" + estado + "]";
 	}
-	
-	
-	
 }
--- Aula-04-Exercicios/src/TesteAnimal.java
+++ Aula-04-Exercicios/src/TesteAnimal.java
@@ -1,10 +1,32 @@
 import javax.swing.JOptionPane;
-
 public class TesteAnimal {
-
 	public static void main(String[] args) {
+		int opcao = 0;
+		String menu = "0 - Sair\n"
+					+ "1 - Comer\n"
+					+ "2 - Domir\n"
+					+ "3 - Correr\n"
+					+ "4 - Morrer";
+		
 		Animal rex = new Animal();
 		JOptionPane.showMessageDialog(null, rex);
-	}
 
+		do {
+			opcao = Integer.valueOf(JOptionPane.showInputDialog(menu));
+			switch (opcao){
+			case 1:
+				rex.comer();
+				break;
+			case 2:
+				rex.dormir();
+				break;
+			case 3:
+				rex.correr();
+				break;
+			case 4:
+				rex.morrer();
+				break;	
+			}
+		} while(opcao!=0);	
+	}
 }
